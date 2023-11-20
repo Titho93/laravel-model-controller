96e73ca5038d7d4df651be9839cdc1f689d0bd73
titho93 <titho93.10@gmail.com>
2023-11-20 16:38:54 +0100

    [add]cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
+    <div class="container d-flex flex-wrap">
+
+        @foreach ($movies as $movie)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <p class="card-text">{{ $movie->title }}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 @endsection
